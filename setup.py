4777785aef34348a1526bb5461dd7d3e7d18f2bd
Nima <nima.manaf8@gmail.com>
2022-05-09 22:47:45 +0200

    add MI-NET

--- setup.py
+++ setup.py
@@ -1,27 +1,65 @@
-import setuptools
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="mil_pytorch",
-    version="0.0.1",
-    author="Jakub Monhart",
-    author_email="monhajak@fel.cvut.cz",
-    description="Model for solving multiple instance learning problem implemented using pytorch",
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+name = "mil"
+version = "0.1"
+release = "0.0.1"
+description = "Multiple Instance Learning"
+author = "Nima"
+author_email = "nima.manaf8@gmail.com"
+url = "nimaman.github.io"
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
+
+dev_requirements = ["black==20.8b1"]
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name=name,
+    version=release,
+    description=description,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jakubmonhart/mil_pytorch",
+    url=url,
+    author=author,
+    author_email=author_email,
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "License :: Other/Proprietary License",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    packages=setuptools.find_packages(),
-    install_requires = [
-        'torch',
-        'scikit-learn'
-    ]
-
+    packages=find_packages(exclude=["doc", "tests"]),
+    #  options for documentation builder
+    command_options={
+        "build_sphinx": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+            "release": ("setup.py", release),
+            "copyright": ("setup.py", copyright),
+            "source_dir": ("setup.py", "doc/source"),
+            "build_dir": ("setup.py", "doc/build"),
+        }
+    },
+    python_requires=">=3.6",
+    setup_requires=["pytest-runner", "setuptools-git-version", "wheel"],
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
 )
